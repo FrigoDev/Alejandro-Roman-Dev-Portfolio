c6efff42b4443deb2559970af14d361d97eafa86
FrigoAlex <alejo012903@hotmail.com>
2023-04-17 03:40:44 -0500

    ✨💫  Add ProgressBar component into the project

---
+++ src/components/ProgressBar/index.tsx
@@ -0,0 +1,18 @@
+import { motion, useScroll, useSpring } from "framer-motion";
+import * as React from "react";
+import "./ProgressBar.css";
+
+const ProgressBar = () => {
+
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+  return (
+    <motion.main style={{ scaleX }} className="progressBar" />
+  );
+};
+export default ProgressBar;
