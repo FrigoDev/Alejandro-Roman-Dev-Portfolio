dadf8936886f915c1a000dd51ebb6d9a850c4eff
FrigoAlex <alejo012903@hotmail.com>
2023-04-18 00:34:55 -0500

    ✨ Add skills section component

---
+++ src/components/SkillsSection/index.tsx
@@ -0,0 +1,63 @@
+import { motion } from "framer-motion";
+import * as React from "react";
+
+import SoftwareGif from "../../images/Softwaregif.webp";
+import { AllContentfulTechnologiesEdge } from "../../types/home";
+import { showAnimation } from "../../utils/animations";
+import MiniCards from "../MiniCards";
+
+const Skills = ({ data }: { data: AllContentfulTechnologiesEdge[] }) => {
+  return (
+    <div className="flex flex-col">
+      <motion.div className="mx-auto">
+        <motion.p
+          viewport={{ once: true, amount: 0.8 }}
+          initial="offscreen"
+          whileInView="onscreen"
+          variants={showAnimation}
+          className="font-semibold text-center"
+        >
+          I’m a frontend developer that loves build beautiful and functional
+          websites and web apps using javascript technologies like React,
+          NextJS, Gatsby, etc.
+        </motion.p>
+        <div className="mt-5">
+          <motion.img
+            viewport={{ once: true, amount: 0.8 }}
+            initial="offscreen"
+            whileInView="onscreen"
+            variants={showAnimation}
+            src={SoftwareGif}
+            alt="Software developer gif"
+            className="rounded-full mx-auto"
+          />
+        </div>
+      </motion.div>
+      <div className="text-center mx-auto">
+        <motion.h3
+          viewport={{ once: true, amount: 0.8 }}
+          initial="offscreen"
+          whileInView="onscreen"
+          variants={showAnimation}
+          className="text-blue-500 text-2xl font-semibold mt-14 mb-7"
+        >
+          MY DEVELOPMENT STACK
+        </motion.h3>
+        <div className="flex flex-row flex-wrap justify-center gap-3">
+          {data.map((edge) => {
+            const { node } = edge;
+            return (
+              <MiniCards
+                key={node.name}
+                name={node.name}
+                image={node.image.gatsbyImageData}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
