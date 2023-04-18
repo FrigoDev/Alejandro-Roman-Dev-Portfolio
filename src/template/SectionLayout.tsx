4cea2345667295b4e8cb1fbd2a427a7e9ca6fe9b
FrigoAlex <alejo012903@hotmail.com>
2023-04-18 00:32:51 -0500

    ✨Add section layout

--- src/template/SectionLayout.tsx
+++ src/template/SectionLayout.tsx
@@ -1,20 +1,46 @@
 import { motion } from "framer-motion";
-import * as React from "react";
-import "./SectionLayout.css";
+import React, { useState, useEffect } from "react";
 
-const SectionLayout = ({ name, children }: { name: string, children: React.ReactNode}) => {
+import "./SectionLayout.css";
+import { showAnimation } from "../utils/animations";
+
+const SectionLayout = ({
+  name,
+  type,
+  children,
+}: {
+  name: string;
+  type: string;
+  children: React.ReactNode;
+}) => {
+  const [myClass, setMyClass] = useState("");
+
+  useEffect(() => {
+    type === "dark" ? setMyClass("section-dark") : setMyClass("");
+  });
+
   return (
-    <section id={name} className="SectionLayout">
+    <section id={name} className={myClass + " pb-14"}>
       <div className="flex flex-col container mx-auto">
         <div className="my-14">
-          <motion.h2 className="text-blue-500 text-center uppercase font-bold text-4xl tracking-wide">
+          <motion.h2
+            viewport={{ once: true, amount: 0.8 }}
+            initial="offscreen"
+            whileInView="onscreen"
+            variants={showAnimation}
+            className="text-blue-500 text-center uppercase font-extrabold text-4xl tracking-wide"
+          >
             {name}
           </motion.h2>
-          <div className="w-1/5 mx-auto my-2 section-divider"/>
+          <motion.div
+            viewport={{ once: true, amount: 0.8 }}
+            initial="offscreen"
+            whileInView="onscreen"
+            variants={showAnimation}
+            className="w-1/5 mx-auto my-2 section-divider"
+          />
         </div>
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
     </section>
   );
