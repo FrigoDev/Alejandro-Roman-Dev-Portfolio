b87c5486f5fe1e650530ef660d40a95e5ec11191
FrigoAlex <alejo012903@hotmail.com>
2024-07-03 02:46:47 -0500

    ✨ Add tooltips into Minicards

--- src/components/MiniCards/index.tsx
+++ src/components/MiniCards/index.tsx
@@ -19,10 +19,17 @@ const MiniCards = ({
       whileHover={hoverEffect}
       variants={showAnimation}
       viewport={{ once: true, amount: 0.8 }}
-      className="flex justify-center items-center my-2 w-1/4 rounded-2xl aspect-square card-bg cursor-pointer"
+      className="relative flex justify-center items-center m-2 rounded-2xl aspect-square card-bg cursor-pointer group"
     >
       <GatsbyImage image={image} alt={name} className="w-1/2" />
+      <div className="absolute bottom-0 flex-col items-center hidden sm:group-hover:flex">
+        <span className="bg-inherit text-white text-base rounded py-1 px-2 mb-1 shadow-md">
+          {name.slice(0, -5)}
+        </span>
+        <div className="w-3 h-3 -mb-1 rotate-45 bg-[#34455d]"></div>
+      </div>
     </motion.div>
   );
 };
+
 export default MiniCards;
