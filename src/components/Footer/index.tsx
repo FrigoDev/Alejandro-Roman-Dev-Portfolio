afa751236bfe536da34236e56598713b85544332
FrigoAlex <alejo012903@hotmail.com>
2024-07-03 02:23:40 -0500

    🐛 Fix footer links only works with logos

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "gatsby";
 import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
@@ -8,22 +7,18 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="flex flex-wrap justify-around items-center bg-footer text-white gap-4 p-6">
-      <div className="flex duration-150 hover:text-blue-500 cursor-pointer">
-        Find me on LinkedIn
-        <Link
-          className="ml-2"
-          to="https://www.linkedin.com/in/alejandro-ramirez-0b0b1b1b9/"
-        >
-          <FaLinkedin className="text-2xl" />
-        </Link>
-      </div>
+      <a href="https://www.linkedin.com/in/frigodev/" target="_blank" rel="noreferrer">
+        <div className="flex duration-150 hover:text-blue-500 cursor-pointer">
+          Find me on LinkedIn
+          <FaLinkedin className="ml-2 text-2xl" />
+        </div>
+      </a>
       <div>© 2023 - Alejandro Román Misas</div>
-      <div className="flex duration-150 hover:text-blue-500 cursor-pointer">
-        Send me an Email{" "}
-        <Link className="ml-2" to="mailto:alejo012903@gmail.com">
-          <MdEmail className="text-2xl" />
-        </Link>
-      </div>
+      <a href="mailto:alejo012903@gmail.com">
+        <div className="flex duration-150 hover:text-blue-500 cursor-pointer">
+          Send me an Email <MdEmail className="ml-2 text-2xl" />
+        </div>
+      </a>
     </div>
   );
 };
